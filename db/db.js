3a749fe007d948fcb38e06e551b00f8dbbfe0cbc
lokesh17t-pixel <lokesh17t@gmail.com>
2026-09-25 23:14:53 +0530

    Add database connection

---
+++ db/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import "dotenv/config"
+
+export default async function dbConnect() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Database connected ✅");
+  } catch (error) {
+    console.log(error);
+  }
+}
